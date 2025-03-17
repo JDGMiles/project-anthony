c00953e38b1f6b2ec7ea45bd9da293d42fa14fd2
James Miles <jimdgmiles@gmail.com>
2025-03-17 06:21:45 +0000

    x

--- code.js
+++ code.js
@@ -1,13 +1,35 @@
 function main() {
 
-  function updateclock() {
-    const d = new Date();
-    document.getElementById("funky").innerHTML = d.getTime();
-    document.getElementById("chicken").innerHTML = 10000*Math.round(d.getTime() % (1000 * 60 * 60 * 24)/10000) / 1000000;
-    setTimeout(updateclock, 100)
-  }
+  const d = new Date();
 
-  updateclock()
+  var millisecondssince01011970 = d.getTime();
+
+  var millisecondstoday = d.getTime() % (1000 * 60 * 60 * 24);
+
+  var secondstoday = millisecondstoday / 1000;
+  var minutestoday = secondstoday / 60;
+  var hourstoday = minutestoday / 60;
+
+  var decimalhourstoday = hourstoday / 2.4;
+  var decimalminutestoday = hourstoday / 0.24;
+  var decimalsecondstoday = hourstoday /0.024;
+
+    document.getElementById("funky1").innerHTML = "Seconds elapsed since Jan 1st 1970:";
+  document.getElementById("chicken1").innerHTML = Math.round(millisecondssince01011970 / 1000000000) + "bn";
+
+  document.getElementById("funky2").innerHTML = "Seconds elapsed today:";
+  document.getElementById("chicken2").innerHTML = Math.floor(secondstoday) + " seconds";
+
+  document.getElementById("funky3").innerHTML = "Coordinated Universal Time:";
+  document.getElementById("chicken3").innerHTML = Math.floor(hourstoday) + "h " + Math.floor(minutestoday) % 60 + "m " + Math.floor(secondstoday) % 60 + "s";
+
+  document.getElementById("funky4").innerHTML = "Metric Time:";
+  document.getElementById("chicken4").innerHTML = Math.floor(secondstoday / 10000) + "Ms " + Math.floor(secondstoday % 10000 / 1000) + "ks " + Math.floor(secondstoday % 1000 / 100) + "hs " + Math.floor(secondstoday % 100 / 10) + "ds " + Math.floor(secondstoday % 10) + "s";
+
+  document.getElementById("funky5").innerHTML = "Decimal Time:";
+  document.getElementById("chicken5").innerHTML = Math.floor(decimalhourstoday) + "h " + Math.floor(decimalminutestoday) + "m " + Math.floor(decimalsecondstoday) + "m";
+
+  setTimeout(main, 100)
 
 }
 
